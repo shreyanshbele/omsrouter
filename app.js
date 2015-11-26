232c6ca28e8d87cc19c62647db3891873b6a8707
shreyanshbele <shreyansh@arvindinternet.com>
2015-11-26 21:44:20 +0530

    Checking

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var app = express();
 
+app.get('/', function (req, res) {
+  res.send('Hello OMS I Will Route Your API!!!!');
+});
+
 var server = app.listen(3300, function () {
 
   console.log("Server Listening...");
